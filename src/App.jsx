5c2c97a44ea2c699bd4964ba07728ddfa382f33e
Dhamodharandeepthi <rdhamodharan22@gmail.com>
2024-01-21 09:43:33 +0530

    newwww

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,13 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import ShortUrlRedirect from "./components/ShortUrlRedirect ";
-import Toggle from "./components/Toggle";
-import { Heading, Text } from "@chakra-ui/react";
-import { InputForm } from "./components/InputForm";
+import React from "react";
+import Navbar from "./component/Navbar/Navbar";
+import Container from "./component/Container/Container";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        
-        <Route path='/:urlCode'
-          element={<ShortUrlRedirect />}
-        />
-      </Routes>
-      <>
-        <Toggle />
-        <Heading as="h3" size="xl" m={'3% 0% 2% 0%'}>
-          URL Shortener
-        </Heading>
-        <InputForm />
-      </>
+    <div className={styles.container}>
+      <Navbar />
+      <Container />
     </div>
   );
 }
